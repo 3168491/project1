2ee7325f757f242de2bd9e75126147149b426dc2
Samuel Murray <c3168491@uon.edu.au>
2019-04-04 08:27:55 +0000

    got started on the project
       added unfiinished encryptionn/ decryption functions
    
       added comments, small amount of variables, attempted to set up array

--- project1.c
+++ project1.c
@@ -1,8 +1,36 @@
 #include <stdio.h>
 
+e(x) == (m + k)(%26)  // will need later for setup of encryption function
+d(c) == (c - k)(%26)  // used for decryption prototype (needs some work)
+      // note that if a negative comes out of either these equations will need to add 26
+      // by adding 26, will become positive
+
+
+      // need to use ascii std 
+      // CAPS ranges from A= 65 to Z= 90
+      // lower from a= 97 to z= 122
+      // will need to subtract 32 to register as a a capital
+
+
+      // might be an idea to resaerch strings
+      
+      
 int main(){
-    
-   printf("Good evening");
+   int k = 1  // the integer k represents the key (the shift in the aplhabet) 
+   char m  // message letter 
+   char c  // cipher text letter
+      // will need to assign integer values to each letter of the alphabet
+      
+      
+   
+   scanf()    // reads the input which will be coded
+char alpha[25] //attempted setup of an array
+   
+   
+   
+    if()   // this should probably go in the functions for encryption and decryption
+          // will be used if m + k or c - k are negative
+          
    
    return 0;
 }
